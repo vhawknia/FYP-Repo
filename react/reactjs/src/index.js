0291bc9ea83277786b01fee9293987992a45334a
victorPeh <victorpehfengwei@gmail.com>
2024-06-22 01:09:43 +0800

    Update index.js

--- react/reactjs/src/index.js
+++ react/reactjs/src/index.js
@@ -7,12 +7,22 @@ import reportWebVitals from './reportWebVitals';
 
 //import Practice from "./Practice";
 
+
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <VoterApp />
   </React.StrictMode>
 );
+*/
+
+
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
